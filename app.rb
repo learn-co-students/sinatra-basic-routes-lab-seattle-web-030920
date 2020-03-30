76560ec6e311fe540ba5ee948059ca67745aa88a
Bakugou808 <janu.m.sung@gmail.com>
2020-03-29 20:36:55 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Janu M. Sung"
+    end
+
+    get '/hometown' do 
+        "My hometown is Cambridge, Massachusetts."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is ... Honestly there are too many."
+    end 
+
 end
