e4bba6de09bcd8a714865f113478a1b83663e8a5
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-03-30 18:34:27 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/name' do 
+        "My name is Gabriel"
+    end
+
+    get '/hometown' do 
+       "My hometown is Uberlandia-MG Brazil"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Metamorfose"
+    end
+
 end
